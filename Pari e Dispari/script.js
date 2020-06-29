1efa4af50dd3c2afdc475936087e1fd972f52d25
Roberto Riccioli <riccioli.rob@gmail.com>
2020-06-29 19:04:06 +0200

    ex P&D (prompt e var)

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,24 @@
+/* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+
+// RICHIESTE ALL'UTENTE
+
+var userChoose = prompt("Fai la tua scelta: Pari o Dispari?");
+
+var userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+console.log(userNumber);
+
+// NUMERO CPU e SOMMA
+
+var cpuNumber = //QUA INSERISCO UNA FUNZIONE!!
+
+console.log(cpuNumber);
+
+var sumNumber = userNumber + cpuNumber;
+
+console.log(sumNumber);
